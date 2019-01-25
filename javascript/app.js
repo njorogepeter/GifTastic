2247d30375a68965239628ed84be74799f11e64b
njorogepeter <pgakonde@yahoo.com>
2019-01-25 12:19:47 -0800

    added animated functions

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,9 @@
     //initial array for animals
-    var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle",
-        "sugar", "glider", "chinchilla", "hedgehog", "hermit", "crab", "gerbil", "pygmy", "goat",
-        "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
+    var animals = ["dog", "cat", "rabbit", "hamster", "skunk", 
+                    "goldfish", "bird", "parrot", "turtle",
+                    "squirrel", "crab", "gerbil", "goat",];
 
-    // Function for dumping the JSON content for each button into the div
-  
-
+// Function for dumping the JSON content for each button into the div
 function displayAnimalInfo(){
         var animal = $(this).attr("data-animal");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -25,13 +23,17 @@ function displayAnimalInfo(){
 
             for(var i = 0; i < results.length; i++) {
 
-                var animalDiv = $("<div>");
+                var animalDiv = $("<div class=gif>");
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
                 
                 
                 animalImage = $("<img>");
-                animalImage.attr("src", results[i].images.fixed_height.url);
+                animalImage.attr("src", results[i].images.fixed_height_still.url);
+                animalImage.attr('data-still', results[i].images.fixed_height_still.url);
+                animalImage.attr('data-animate', results[i].images.fixed_height.url);
+                animalImage.attr('data-state', 'still');
+                animalImage.addClass('gif');
                 
                 animalDiv.append(p);
                 animalDiv.append(animalImage);
@@ -41,6 +43,21 @@ function displayAnimalInfo(){
                 console.log(rating);
 
             }
+            $(".gif").on("click", function(){
+                var state = $(this).attr("data-state");
+                console.log(state); 
+            
+                if(state === 'still'){
+                  var dataAnimate = $(this).attr("data-animate");
+                  $(this).attr("src", dataAnimate);
+                  $(this).attr('data-state','animate' );
+                } else {
+                  var dataStill = $(this).attr("data-still");
+                  $(this).attr("src", dataStill);
+                  $(this).attr('data-state','still' );
+                }
+            
+            });
 
         });
 
@@ -72,6 +89,10 @@ function displayAnimalInfo(){
 
         renderButtons();
     });
-renderButtons();
 
+
+
+$(document).on("click", ".animal", displayAnimalInfo);
+
+renderButtons();
 
